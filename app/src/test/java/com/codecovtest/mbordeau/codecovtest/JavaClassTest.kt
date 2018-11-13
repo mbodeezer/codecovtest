b0d23f307506ce8265795269ed0d33e4f4ce3f34
MBordeau <mbo@deezer.com>
2018-11-13 11:52:37 +0100

    test 3

---
+++ app/src/test/java/com/codecovtest/mbordeau/codecovtest/JavaClassTest.kt
@@ -0,0 +1,17 @@
+package com.codecovtest.mbordeau.codecovtest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class JavaClassTest {
+    @Test
+    fun addition_isCorrect() {
+        JavaClass().add(4, 3)
+    }
+}
