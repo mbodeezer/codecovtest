e04dbc08c6182a2fe95b2fbee1d9abae14b70d51
MBordeau <mbo@deezer.com>
2018-11-13 11:50:25 +0100

    test 2

---
+++ app/src/main/java/com/codecovtest/mbordeau/codecovtest/JavaClass.java
@@ -0,0 +1,8 @@
+package com.codecovtest.mbordeau.codecovtest;
+
+public class JavaClass {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ app/src/main/java/com/codecovtest/mbordeau/codecovtest/KotlinClass.kt
@@ -0,0 +1,6 @@
+package com.codecovtest.mbordeau.codecovtest
+
+class KotlinClass {
+
+    fun add(a: Int, b: Int) = a + b
+}
